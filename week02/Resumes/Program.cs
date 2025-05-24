dbea6aa6cf9c18230d6d7f4f6d05ccdce15b5447
franciscorodriguezsv24 <franciscorodriguezsv24@gmail.com>
2025-05-23 21:07:54 -0600

    added fraction exercises

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,44 @@
+
 using System;
 
+// Abstract class defining common interface
+public abstract class MusicPlayer
+{
+    public abstract void Play();
+    public abstract void Pause();
+}
+
+// Implementation for MP3 files
+public class MP3Player : MusicPlayer
+{
+    private string fileName;
+    
+    public MP3Player(string fileName) { this.fileName = fileName; }
+    
+    public override void Play() => Console.WriteLine($"Playing MP3: {fileName}");
+    public override void Pause() => Console.WriteLine("Pausing MP3");
+}
+
+// Implementation for streaming
+public class StreamingPlayer : MusicPlayer
+{
+    private string url;
+    
+    public StreamingPlayer(string url) { this.url = url; }
+    
+    public override void Play() => Console.WriteLine($"Streaming from: {url}");
+    public override void Pause() => Console.WriteLine("Pausing stream");
+}
+
+// Using abstraction
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        MusicPlayer mp3 = new MP3Player("song.mp3");
+        MusicPlayer stream = new StreamingPlayer("radio.com");
+        
+        mp3.Play();
+        stream.Play();
     }
 }
